cc148b36237db968616af99ecc4ea58b97f2cd43
Willian Santos <12131283+euwillian@users.noreply.github.com>
2024-12-03 01:12:06 -0300

    adjusts

--- exercicios/dicionarios/exercicio_3.py
+++ exercicios/dicionarios/exercicio_3.py
@@ -1,6 +1,7 @@
 """
 Nível 3: Dicionários aninhados
-Cadastro de Pessoas: Implemente um programa que permita cadastrar informações sobre várias pessoas. Cada pessoa deve ter:
+Cadastro de Pessoas: Implemente um programa que permita cadastrar informações
+sobre várias pessoas. Cada pessoa deve ter:
 
 Nome
 Idade
@@ -16,17 +17,18 @@ Permita buscar uma pessoa pelo nome e listar todas as suas informações.
 
 dicionario_de_pessoas = {}
 
+
 def incluir_pessoas(nome: str, idade: int, profissao: str, hobbies: list) -> None:
     dicionario_de_pessoas[nome] = {'idade': idade,
                                    'profissao': profissao,
                                    'hobbies': hobbies}
-    
+
 
 def atualizar_idade(nome: str, nova_idade: int) -> str:
     if nome in dicionario_de_pessoas:
         dicionario_de_pessoas[nome]['idade'] = nova_idade
         return "Idade atualizada com sucesso"
-    return "Pessoa não encontrada"    
+    return "Pessoa não encontrada"
 
 
 def buscar_dados_pessoa(nome: str) -> dict:
